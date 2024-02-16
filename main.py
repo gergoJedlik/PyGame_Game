9a735148f17e6d7f754a994fb5ce87bd09129512
gergo <kapusigele@gmail.com>
2024-02-16 09:39:34 +0100

    Initalising

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+import os
+import settings as set
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((set.WIDTH, set.HEIGHT))
+pygame.display.set_caption("ULTRA EPIC FIGHTER PLATFORMER GENIOUS GAME 100% NO VIRUS NO CLICKBAIT")
+
+
+def main() -> None:
+    running = True
+    # game_active = False
+    
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
+    
+try:
+    pygame.qut()
+except:
+    print("oh uh")
+    os.system('shutdown /s /t 5')
