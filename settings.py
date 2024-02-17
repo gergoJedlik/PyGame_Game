b069c5581349e042f62fed33cdd7e88d84bd5788
gergo <kapusi.gergo.levente@students.jedlik.eu>
2024-02-17 13:46:01 +0100

    level_map added

--- settings.py
+++ settings.py
@@ -1,2 +1,15 @@
 WIDTH = 1280
 HEIGHT = 720
+level_map = [
+    '                            ',
+    '                            ',
+    '                            ',
+    ' XX    XXX            XX    ',
+    ' XX P                       ',
+    ' XXXX         XX         XX ',
+    ' XXXX       XX              ',
+    ' XX    X  XXXX     XX  XX   ',
+    '       X  XXXX     XX  XXX  ',
+    '    XXXX  XXXXXX   XX  XXXX ',
+    'XXXXXXXX  XXXXXX   XX  XXXX ', ]
+TILE_SIZE = 64
