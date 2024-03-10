a32be5e2ddd8f079326091fe17ab944eaa3bb492
gergo <kapusi.gergo.levente@students.jedlik.eu>
2024-03-10 22:09:03 +0100

    Merge of START_FROM_SCRATCH main.py

--- main.py
+++ main.py
@@ -16,49 +16,96 @@ def main() -> None:
 
     level = Level(sett.LEVEL_MAP_STR)
     floor = level.get_objects
-    
+
+    bg_dict = draw()
+    menu_dict = menu()
+
+    active: bool = False
     running = True
     while running:
-         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not player1.P_attack and not player1.dead:
-                    player1.attack()
-                if event.key == pygame.K_RSHIFT and not player2.P_attack and not player2.dead:
-                    player2.attack()
-                if event.key == pygame.K_w and not player1.P_attack and player1.jump_count < 1 and not player1.dead:
-                    player1.jump()
-                if event.key == pygame.K_UP and not player2.P_attack and player2.jump_count < 1 and not player2.dead:
-                    player2.jump()
-                if event.key == pygame.K_LSHIFT and not player1.P_attack and player1.dash_cd == 0 and not player1.dead:
-                    player1.dash()
-                if event.key == pygame.K_MINUS  and not player2.P_attack  and player2.dash_cd == 0 and not player2.dead:
-                    player2.dash()
-                
+
+        if active is True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE and not player1.P_attack and not player1.dead:
+                        player1.attack()
+                    if event.key == pygame.K_RSHIFT and not player2.P_attack and not player2.dead:
+                        player2.attack()
+                    if event.key == pygame.K_w and not player1.P_attack and player1.jump_count < 1 and not player1.dead:
+                        player1.jump()
+                    if event.key == pygame.K_UP and not player2.P_attack and player2.jump_count < 1 and not player2.dead:
+                        player2.jump()
+                    if event.key == pygame.K_LSHIFT and not player1.P_attack and player1.dash_cd == 0 and not player1.dead:
+                        player1.dash()
+                    if event.key == pygame.K_MINUS  and not player2.P_attack  and player2.dash_cd == 0 and not player2.dead:
+                        player2.dash()
 
 
-        player1.loop(sett.FPS)
-        player2.loop(sett.FPS)
+            player1.loop(sett.FPS)
+            player2.loop(sett.FPS)
 
 
-        handle_movement(player1, player2, floor)
+            handle_movement(player1, player2, floor)
 
-        handle_hit(player1, player2)
+            handle_hit(player1, player2)
 
-        bg_surface, bg_rect = draw()
-        update(screen, bg_surface, bg_rect, player1, player2, floor)
+            update(screen, bg_dict, player1, player2, floor)
 
 
-        clock.tick(sett.FPS)
+            clock.tick(sett.FPS)
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            screen.fill((0, 0, 0))
+            for value in menu_dict.values():
+                screen.blit(value[0], value[1])
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_SPACE]:
+                active = True
+            pygame.display.update()
+
+def menu() -> dict[str, tuple[pygame.Surface, pygame.Rect]]:
+    menu_dict = {}
+
+    title_font = pygame.font.Font(os.path.join("Assets", "DigitalDisco.ttf"), 128)
+    font = pygame.font.Font(os.path.join("Assets", "DigitalDisco.ttf"), 32)
+    text: pygame.Surface = title_font.render("GAME NAME", True, (255, 255, 255), None)
+    textRect: pygame.Rect = text.get_rect(center=(sett.WIDHT//2, sett.HEIGHT//2))
+    menu_dict["game name"] = (text, textRect)
+
+    text: pygame.Surface = font.render("PRESS 'SPACE' TO PLAY", True, (255, 255, 255), None)
+    textRect: pygame.Rect = text.get_rect(center=(sett.WIDHT//2, (sett.HEIGHT//10)*9))
+    menu_dict['press to play'] = (text, textRect)
+
+    return menu_dict
         
 
-def draw():
+def draw() -> dict[str, tuple[pygame.Surface, pygame.Rect]]:
+    bg_dict = {}
+
     bg_surface = pygame.image.load(os.path.join("Assets", "Tileset", "Background_0.png")).convert()
     bg_surface = pygame.transform.scale(bg_surface, (sett.WIDHT, sett.HEIGHT))
     bg_rect = bg_surface.get_rect(bottomleft=(0, sett.HEIGHT))
-    return bg_surface, bg_rect
+    bg_dict["background"] = (bg_surface, bg_rect)
+
+    bg_cemetery = pygame.image.load(os.path.join("Assets", "Tileset", "Background_1.png")).convert_alpha()
+    bg_cemetery = pygame.transform.scale(bg_cemetery, (sett.WIDHT, sett.HEIGHT))
+    bg_crect = bg_surface.get_rect(bottomleft=(0, sett.HEIGHT+75))
+    bg_dict["cemetery"] = (bg_cemetery, bg_crect)
+
+    for placement in range((sett.WIDHT//352)+1):
+        if placement == 0:
+            bg_grass = pygame.image.load(os.path.join("Assets", "Tileset", "Grass_background_2.png")).convert_alpha()
+        else:
+            bg_grass = pygame.image.load(os.path.join("Assets", "Tileset", "Grass_background_1.png")).convert_alpha()
+        bg_g_rect = bg_grass.get_rect(bottomleft=(352*placement, sett.HEIGHT+30))
+        bg_dict[f"grass_{placement}"] = (bg_grass, bg_g_rect)
+    
+    return bg_dict
 
 
 def handle_movement(player1: Player, player2: Player, objects):
@@ -133,8 +180,10 @@ def handle_vertical_collision(player1: Player, player2: Player, objects: list[Ti
             collided_objs.append(obj)
             
 
-def update(screen: pygame.Surface, bg_surface, bg_rect, player1: Player, player2: Player, floor,):
-    screen.blit(bg_surface, bg_rect)
+def update(screen: pygame.Surface, bg_dict: dict[str, tuple[pygame.Surface, pygame.Rect]], player1: Player, player2: Player, floor,):
+    for value in bg_dict.values():
+        screen.blit(value[0], value[1])
+
 
     # Healthbar Lenght Update
     health1_bg = pygame.Rect(47, 67, 366, 31)
@@ -146,9 +195,7 @@ def update(screen: pygame.Surface, bg_surface, bg_rect, player1: Player, player2
     
     player1.display_name[1].bottomleft = health1_bg.topleft
     player1.display_name[1].left += 8
-    player1.display_name[1].bottom -= int(6.9) - int(6.9-4.20) * int(6.9/(6.9/3))
     player2.display_name[1].bottomright = health2_bg.topright
-    player2.display_name[1].bottom -= int(6.9) - int(6.9-4.20) * int(6.9/(6.9/3))
     player2.display_name[1].left -= 8
     
     screen.blit(player1.display_name[0], player1.display_name[1])
@@ -159,12 +206,12 @@ def update(screen: pygame.Surface, bg_surface, bg_rect, player1: Player, player2
 
 
     # SHOW HITBOXES
-    #     pygame.draw.rect(screen, (0, 0, 255), obj.collidebox, 5)
+       # pygame.draw.rect(screen, (0, 0, 255), obj.collidebox, 5)
     # pygame.draw.rect(screen, (0, 255, 0), player1.hitbox, 3)
     # pygame.draw.rect(screen, (0, 255, 0), player2.hitbox, 3)
-    # # if player1.attackbox_active:
+    # if player1.attackbox_active:
     # pygame.draw.rect(screen, (255, 0, 0), player1.attackbox, 3)
-    # # if player2.attackbox_active:
+    # if player2.attackbox_active:
     # pygame.draw.rect(screen, (255, 0, 0), player2.attackbox, 3)
     
     player1.draw(screen)
